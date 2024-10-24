f63fa5dba7fc0f760910a9e88685078a5b488e21
Michael Horwitz <michael@horwitz.co.za>
2024-10-24 13:11:06 +0200

    progress on intermediate code generation

--- ICG.java
+++ ICG.java
@@ -8,37 +8,48 @@ public class ICG {
     Pattern numberPattern = Pattern.compile("-?[0-9]+(\\.[0-9]+)?");
     int currPlace;
     int currLabel;
-    public class ReturnCodeArgs{
+
+    public class ReturnCodeArgs {
         public StringBuilder code;
         public ArrayList<StringBuilder> argList;
     }
-    public ICG(){
+
+    public ICG() {
         currPlace = 0;
         currLabel = 0;
         symbolTable = new SymbolTable();
         rootNode = new Node("PROG");
     }
-    public ICG(SymbolTable st, Node rn){
+
+    public ICG(SymbolTable st, Node rn) {
         symbolTable = st;
         rootNode = rn;
         currPlace = 0;
         currLabel = 0;
     }
-    public StringBuilder newVar(){
+
+    public StringBuilder newVar() {
         StringBuilder sb = new StringBuilder("var");
         return sb.append(currPlace++);
     }
+
     public StringBuilder newLabel() {
         StringBuilder sb = new StringBuilder("lab");
         return sb.append(currLabel++);
     }
-    public StringBuilder transOp(StringBuilder operator){
-        if (operator.toString().equals("and")){
+
+    public StringBuilder transOp(StringBuilder operator) {
+        if (operator.toString().equals("and")) {
             return new StringBuilder("&");
         }
         return new StringBuilder();
     }
-    public StringBuilder TransExp(Node exp, StringBuilder place){
+
+    public StringBuilder TransExp(Node exp, StringBuilder place) {
+        if (exp.NodeName.equals("VNAME")) {
+            VariableProps vp = symbolTable.table.get(exp.id);
+            return new StringBuilder(vp.translatedName);
+        }
         if (exp.NodeName.equals("CONST")) {
             if (numberPattern.matcher(exp.NodeName).matches()) {
                 int v = Integer.parseInt(exp.NodeName);
@@ -63,7 +74,7 @@ public class ICG {
                 StringBuilder op = transOp(new StringBuilder(opNode.childNodes.getFirst().NodeName));
                 return code1.append(place).append(":=").append(op).append(place1);
             }
-            if (opNode.NodeName.equals("BINOP")){
+            if (opNode.NodeName.equals("BINOP")) {
                 StringBuilder place1 = newVar();
                 StringBuilder place2 = newVar();
                 StringBuilder code1 = TransExp(exp.childNodes.get(2), place1);
@@ -74,6 +85,7 @@ public class ICG {
         }
         return new StringBuilder();
     }
+
     public ReturnCodeArgs TransExp(Node exp) {
         StringBuilder place = newVar();
         StringBuilder code1 = TransExp(exp, place);
@@ -83,25 +95,33 @@ public class ICG {
         return ret;
     }
 
-    public StringBuilder TransBranch(Node branch){
+    public StringBuilder TransBranch(Node branch) {
         StringBuilder label1 = newLabel();
         StringBuilder label2 = newLabel();
         StringBuilder label3 = newLabel();
         StringBuilder code1 = transCond(branch.childNodes.get(1));
         StringBuilder code2 = transAlgo(branch.childNodes.get(3));
         StringBuilder code3 = transAlgo(branch.childNodes.get(3));
-        return code1.append("LABEL ").append(label1).append(code2).append("GOTO ").append(label3).append("LABEL ").append(label2).append(code3).append("LABEL ").append(label3);
+        return code1.append("LABEL ").append(label1).append(code2).append("GOTO ").append(label3).append("LABEL ")
+                .append(label2).append(code3).append("LABEL ").append(label3);
     }
-    public StringBuilder TransCommand(Node commandNode){
-        if(commandNode.childNodes.getFirst().NodeName.equals("BRANCH")){
+
+    public StringBuilder TransCommand(Node commandNode) {
+        if (commandNode.childNodes.getFirst().NodeName.equals("BRANCH")) {
             return TransBranch(commandNode);
         }
         return new StringBuilder();
     }
-    public StringBuilder transCond(Node node){
+
+    public StringBuilder transCond(Node node) {
         return new StringBuilder();
     }
-    public StringBuilder transAlgo(Node node){
+
+    public StringBuilder transCond(Node cond, StringBuilder labelt, StringBuilder labelf){
+        return new StringBuilder();
+    }
+
+    public StringBuilder transAlgo(Node node) {
         return new StringBuilder();
     }
 }
