39b3b643b72a2c629a77c43fea084aab87ef9d03
Michael Horwitz <michael@horwitz.co.za>
2024-10-24 20:36:16 +0200

    more icg

--- ICG.java
+++ ICG.java
@@ -32,96 +32,171 @@ public class ICG {
         StringBuilder sb = new StringBuilder("var");
         return sb.append(currPlace++);
     }
-
+    public String lookupInTable(Node node){
+        return symbolTable.table.get(node.id).translatedName;
+    }
     public StringBuilder newLabel() {
         StringBuilder sb = new StringBuilder("lab");
         return sb.append(currLabel++);
     }
-
-    public StringBuilder transOp(StringBuilder operator) {
-        if (operator.toString().equals("and")) {
-            return new StringBuilder("&");
+    public StringBuilder transAlgo(Node algoNode){
+        return transInstruc(algoNode.childNodes.get(1));
+    }
+    public StringBuilder transAtomic(Node atomicNode){
+        String firstName = atomicNode.childNodes.getFirst().NodeName;
+        if (firstName.equals("VNAME")) {
+            return new StringBuilder(symbolTable.table.get(atomicNode.childNodes.getFirst().id).translatedName);
+        }
+        if (firstName.equals("CONST")) {
+            return transConst(atomicNode.childNodes.getFirst());
         }
         return new StringBuilder();
     }
-
-    public StringBuilder TransExp(Node exp, StringBuilder place) {
-        if (exp.NodeName.equals("VNAME")) {
-            VariableProps vp = symbolTable.table.get(exp.id);
-            return new StringBuilder(vp.translatedName);
-        }
-        if (exp.NodeName.equals("CONST")) {
-            if (numberPattern.matcher(exp.NodeName).matches()) {
-                int v = Integer.parseInt(exp.NodeName);
-                StringBuilder sb = new StringBuilder();
-                sb.append(place);
-                sb.append(":=");
-                sb.append(v);
-                return sb;
-            } else {
-                StringBuilder sb = new StringBuilder();
-                sb.append(place);
-                sb.append(":=");
-                sb.append(exp.NodeName);
-                return sb;
-            }
-        }
-        if (exp.NodeName.equals("COMPOSIT")) {
-            Node opNode = exp.childNodes.getFirst();
-            if (opNode.NodeName.equals("UNOP")) {
-                StringBuilder place1 = newVar();
-                StringBuilder code1 = TransExp(exp.childNodes.get(2), place1);
-                StringBuilder op = transOp(new StringBuilder(opNode.childNodes.getFirst().NodeName));
-                return code1.append(place).append(":=").append(op).append(place1);
-            }
-            if (opNode.NodeName.equals("BINOP")) {
-                StringBuilder place1 = newVar();
-                StringBuilder place2 = newVar();
-                StringBuilder code1 = TransExp(exp.childNodes.get(2), place1);
-                StringBuilder code2 = TransExp(exp.childNodes.get(4), place2);
-                StringBuilder op = transOp(new StringBuilder(opNode.childNodes.getFirst().NodeName));
-                return code1.append(code2).append(place).append(":=").append(op).append(place2);
-            }
+    public StringBuilder transConst(Node constNode){
+        return new StringBuilder(constNode.childNodes.getFirst().NodeName);
+    }
+    public StringBuilder transArg(Node argNode){
+        return new StringBuilder();
+    }
+    public StringBuilder transUnop(Node unopNode){
+        switch (unopNode.childNodes.getFirst().NodeName) {
+            case "not":
+                return new StringBuilder("!");
+            case "sqrt":
+                return new StringBuilder("sqrt");
         }
         return new StringBuilder();
     }
-
-    public ReturnCodeArgs TransExp(Node exp) {
-        StringBuilder place = newVar();
-        StringBuilder code1 = TransExp(exp, place);
-        ReturnCodeArgs ret = new ReturnCodeArgs();
-        ret.argList.add(place);
-        ret.code = code1;
+    public StringBuilder transBinop(Node binopNode){
+        switch (binopNode.childNodes.getFirst().NodeName) {
+            case "and":
+                return new StringBuilder("AND");
+            case "or":
+                return new StringBuilder("OR");
+            case "eq":
+                return new StringBuilder("=");
+            case "grt":
+                return new StringBuilder(">");
+            case "add":
+                return new StringBuilder("+");
+            case "sub":
+                return new StringBuilder("-");
+            case "mul":
+                return new StringBuilder("*");
+            case "div":
+                return new StringBuilder("/");
+        }
+        return new StringBuilder();
+    }
+    public StringBuilder transOp(Node opNode, String place){
+        String firstName = opNode.childNodes.getFirst().NodeName;
+        if (firstName.equals("UNOP")) {
+            StringBuilder place1 = newVar();
+            StringBuilder code1 = transArg(opNode.childNodes.get(2));
+            StringBuilder unop = transUnop(opNode.childNodes.getFirst());
+            return code1.append("\n").append(place).append(":=").append(unop).append("()").append(place1).append(")x`\n");
+        }
+        if(firstName.equals("BINOP")){
+            StringBuilder place1 = newVar();
+            StringBuilder place2 = newVar();
+            StringBuilder code1 = transArg(opNode.childNodes.get(2));
+            StringBuilder code2 = transArg(opNode.childNodes.get(4));
+            StringBuilder binop = transBinop(opNode.childNodes.getFirst());
+            return code1.append("\n").append(code2).append("\n").append(place).append(" := ").append(place1).append(" ").append(binop).append(" ").append(place2).append("\n");
+        }
+        return new StringBuilder();
+    }
+    public StringBuilder transTerm(Node termNode){
+        String firstName = termNode.childNodes.getFirst().NodeName;
+        if (firstName.equals("ATOMIC")) {
+            return transAtomic(termNode.childNodes.getFirst());
+        }
+        if (firstName.equals("CALL")) {
+            return transCall(termNode.childNodes.getFirst());
+        }
+        if (firstName.equals("OP")) {
+            return transOp(termNode.childNodes.getFirst(), newVar().toString());
+        }
+        return new StringBuilder();
+    }
+    public StringBuilder transAssign(Node assignNode){
+        String op = assignNode.childNodes.get(1).NodeName;
+        if (op.equals("<")) {
+            StringBuilder code1 = transVname(assignNode.childNodes.get(2));
+            return new StringBuilder("INPUT ").append(code1).append("\n");
+        }
+        if (op.equals("=")) {
+            StringBuilder place = newVar();
+            StringBuilder x = transVname(assignNode.childNodes.get(0));
+            StringBuilder transedTerm = transTerm(assignNode.childNodes.getLast());
+            return transedTerm.append(x).append(":=").append(place).append("\n");
+        }
+        return new StringBuilder();
+    }
+    public StringBuilder transVname(Node vNameNode){
+        return new StringBuilder(symbolTable.table.get(vNameNode.id).translatedName);
+    }
+    public StringBuilder transCall(Node callNode){
+        StringBuilder ret = new StringBuilder("CALL_");
+        ret.append(lookupInTable(callNode.childNodes.getFirst()));
+        ret.append("(");
+        ret.append(transAtomic(callNode.childNodes.get(2)));
+        ret.append(",");
+        ret.append(transAtomic(callNode.childNodes.get(4)));
+        ret.append(",");
+        ret.append(transAtomic(callNode.childNodes.get(6)));
+        ret.append(")\n");
         return ret;
     }
-
-    public StringBuilder TransBranch(Node branch) {
-        StringBuilder label1 = newLabel();
-        StringBuilder label2 = newLabel();
-        StringBuilder label3 = newLabel();
-        StringBuilder code1 = transCond(branch.childNodes.get(1));
-        StringBuilder code2 = transAlgo(branch.childNodes.get(3));
-        StringBuilder code3 = transAlgo(branch.childNodes.get(3));
-        return code1.append("LABEL ").append(label1).append(code2).append("GOTO ").append(label3).append("LABEL ")
-                .append(label2).append(code3).append("LABEL ").append(label3);
+    public StringBuilder transBranch(Node callNode){
+        
+        return new StringBuilder();
     }
-
-    public StringBuilder TransCommand(Node commandNode) {
-        if (commandNode.childNodes.getFirst().NodeName.equals("BRANCH")) {
-            return TransBranch(commandNode);
+    public StringBuilder transCommand(Node commandNode){
+        String firstName = commandNode.childNodes.getFirst().NodeName;
+        if (firstName.equals("skip")) {
+            return new StringBuilder("REM DO NOTHING\n");
+        }
+        if (firstName.equals("halt")) {
+            return new StringBuilder("STOP\n");
+        }
+        if (firstName.equals("print")) {
+            StringBuilder code1 = transAtomic(commandNode.childNodes.get(1));
+            return new StringBuilder("PRINT ").append(code1).append("\n");
+        }
+        if (firstName.equals("ATOMIC")) {
+            return new StringBuilder("FUNCTIONS NOT IMPLEMENTED\n");
+        }
+        if (firstName.equals("ASSIGN")) {
+            return transAssign(commandNode.childNodes.getFirst());
+        }
+        if (firstName.equals("CALL")) {
+            return transCall(commandNode.childNodes.getFirst());
+        }
+        if (firstName.equals("BRANCH")) {
+            return transBranch(commandNode.childNodes.getFirst());
+        }
+        
+        return new StringBuilder();
+    }
+    public StringBuilder transInstruc(Node instrucNode){
+        //Instruc null
+        if (instrucNode.childNodes.size() == 0) {
+            return new StringBuilder("REM END \n");
+        }
+        if (instrucNode.childNodes.size() == 3) {
+            StringBuilder code1 = transCommand(instrucNode.childNodes.get(0));
+            StringBuilder code2 = transInstruc(instrucNode.childNodes.getLast());
+            return code1.append(code2);
         }
         return new StringBuilder();
-    }
 
-    public StringBuilder transCond(Node node) {
-        return new StringBuilder();
     }
+    public StringBuilder transProg(Node prognode){
+        StringBuilder code = new StringBuilder("");
+        code.append(prognode.childNodes.get(2));
+        code.append("STOP\n");
 
-    public StringBuilder transCond(Node cond, StringBuilder labelt, StringBuilder labelf){
-        return new StringBuilder();
-    }
-
-    public StringBuilder transAlgo(Node node) {
-        return new StringBuilder();
+        return code;
     }
 }
